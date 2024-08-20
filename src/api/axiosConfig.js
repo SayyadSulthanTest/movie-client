330e47e64d88cba188c7fd6c403fad4efaf51fbd
Sayyadsulthan <asulthan088@gmail.com>
2024-08-20 10:33:25 +0530

    Added the cors method for api call

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,4 +4,13 @@ export default axios.create({
     // baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io/',
     baseURL: apiUrl,
     // headers: { 'ngrok-skip-browser-waring': 'true' },
+    headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+        {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+        },
+    ],
 });
